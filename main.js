6225512266daef9ab0d0bdcd60b558cd8773bb27
Laila-front-dev <your.lailaben@gmail.com>
2024-11-17 20:17:45 +0100

    update file

--- main.js
+++ main.js
@@ -41,3 +41,19 @@ rightArrow.addEventListener("click", () => {
     slider.style.transform = "";
   }, 490);
 });
+
+// SCROLL Animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+hiddenElements.forEach((el) => observer.observe(el));
